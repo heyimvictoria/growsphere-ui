51c7909518e44f774d8cea16483829713a977a08
stephenmdevine <stephenmdevine@gmail.com>
2024-03-20 23:26:28 -0500

    Devine setup

--- grow-sphere/src/App.js
+++ grow-sphere/src/App.js
@@ -6,17 +6,22 @@ import Register from './components/user/Register';
 import Dashboard from './components/user/Dashboard';
 import Home from './components/Home';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/* Define more routes as needed */}
-            </Routes>
-        </Router>
+        // <Router>
+        //     <Routes>
+        //         <Route path="/" element={<Home />} />
+        //         <Route path="/login" element={<Login />} />
+        //         <Route path="/register" element={<Register />} />
+        //         <Route path="/dashboard" element={<Dashboard />} />
+        //         {/* Define more routes as needed */}
+        //     </Routes>
+        // </Router>
+        <div>
+            <NavBar/>
+        </div>
     );
 }
 
