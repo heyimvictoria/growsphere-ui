7ed92c00fc4a6650e7d3dfffee53fc40ab26cd38
SamGoessling <samwyz@icloud.com>
2024-03-25 19:21:00 -0500

    weather link added in nav

--- grow-sphere/src/components/NavBar.js
+++ grow-sphere/src/components/NavBar.js
@@ -6,14 +6,15 @@ const NavBar = () => {
         <nav>
             <ul>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li> {/* Optional, can remove if not using login */}
-                <li><Link to="/register">Register</Link></li> {/* Optional, can remove if not using registration */}
-                <li><Link to="/dashboard">Dashboard</Link></li> {/* Adjust based on actual content */}
-                <li><Link to="/my-plants">My Plants</Link></li> {/* Adjust based on actual content */}
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/register">Register</Link></li>
+                <li><Link to="/dashboard">Dashboard</Link></li>
+                <li><Link to="/my-plants">My Plants</Link></li>
+                <li><Link to="/weather">Weather</Link></li>
                 <li><Link to="/guides">Plant Care Guides</Link></li>
                 <li><Link to="/community">Community</Link></li>
-                <li><Link to="/profile">Profile</Link></li> {/* Optional, can remove if not using profiles */}
-                <li><Link to="/logout">Logout</Link></li> {/* Optional, can remove if not using logout */}
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link to="/logout">Logout</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
