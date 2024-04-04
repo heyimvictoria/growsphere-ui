13ceca896dbf8d86066cf1a64b97cdc7cb5db67e
Travaughn33 <travaughnwatson2@gmail.com>
2024-04-04 01:26:23 -0500

    Functional Contact Form

--- grow-sphere/src/App.js
+++ grow-sphere/src/App.js
@@ -10,36 +10,9 @@ import './index.css';
 import {AuthProvider} from "./context/AuthContext";
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ProfileEdit from './components/user/ProfileEdit';
-import ViewUser from './components/user/ViewUser';
-import {ContactForm}from './components/ContactForm';
 
 function App() {
     return (
-<<<<<<< HEAD
-        // <Router>
-        //     <Routes>
-        //         <Route path="/" element={<Home />} />
-        //         <Route path="/login" element={<Login />} />
-        //         <Route path="/register" element={<Register />} />
-        //         <Route path="/dashboard" element={<Dashboard />} />
-        //         {/* Define more routes as needed */}
-        //     </Routes>
-        // </Router>
-        <div className='App'>
-            <Router>
-                <NavBar/>
-                <Routes>
-                    <Route exact path='/' element={<Home />}/>
-                    <Route exact path='/register' element={<Register />}/>
-                    <Route exact path='/editUser/:id' element={<ProfileEdit />}/>
-                    <Route exact path='/viewUser/:id' element={<ViewUser />}/>
-                    <Route exact path='/contact' element={<ContactForm />}/>
-                </Routes>
-
-            </Router>
-        </div>
-=======
         <AuthProvider>
             <Router>
             <NavBar />
@@ -52,7 +25,6 @@ function App() {
                 </Routes>
             </Router>
         </AuthProvider>
->>>>>>> 8fd289fdb4a150bc6ee45b928198a45b3c7b2931
     );
 }
 
--- grow-sphere/src/components/Home.js
+++ grow-sphere/src/components/Home.js
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Link, useParams } from 'react-router-dom';
-
-export default function Home() {
-
-    const [users,setUsers]=useState([]);
-
-    const {id} = useParams();
-
-    useEffect(() => {
-      loadUsers();
-
-    },[]);
-
-    const loadUsers=async() => {
-      const result = await axios.get("http://localhost:8080/users");
-      setUsers(result.data);
-    }
-
-    const deleteUser = async (id) => {
-      await axios.delete(`http://localhost:8080/user/${id}`)
-      loadUsers();
-    }
-
-  return (
-    <div className='container'>
-        <div className='py-4'>
-        <table className="table border shadow">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Username</th>
-      <th scope="col">Email</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>
-
-    {
-      users.map((user, index) => (
-        <tr>
-          <th scope="row" key={index}>{index + 1}</th>
-          <td>{user.username}</td>
-          <td>{user.email}</td>
-          <td>
-            <Link className='btn btn-primary mx-2' to={`/viewUser/${user.id}`}>View</Link>
-            <Link className='btn btn-outline-primary mx-2' to={`/editUser/${user.id}`}>Edit</Link>
-            <button className='btn btn-primary mx-2' onClick={()=>deleteUser(user.id)}>Delete</button>
-          </td>
-        </tr>
-      ))
-    }
-  </tbody>
-</table>
-        </div>
-    </div>
-  )
-}
-=======
 import React from 'react';
 
 const Home = () => {
@@ -66,4 +5,3 @@ const Home = () => {
 };
 
 export default Home;
->>>>>>> 8fd289fdb4a150bc6ee45b928198a45b3c7b2931
