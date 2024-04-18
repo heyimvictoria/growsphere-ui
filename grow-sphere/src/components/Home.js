198393dbed3684a55c6f2e2012cfd70e6d5667f2
Travaughn33 <travaughnwatson2@gmail.com>
2024-04-18 18:40:11 -0500

    RESTAPI deleted.

--- grow-sphere/src/components/Home.js
+++ grow-sphere/src/components/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ContactFormRestAPI from './ContactFormRestAPI'
 import ContactForm from './ContactForm'
 
 const Home = () => {
     return <div><h1>Welcome to GrowSphere</h1>
     <h2>Contact Support</h2>
     <ContactForm></ContactForm>
-
     </div>
     ;
     
